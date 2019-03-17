f8dfc467f255602712a9c1a72b7be6c124294212
kekDev <klientov76@mail.ru>
2019-03-17 11:33:00 +0300

    'sec'

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ from werkzeug.security import generate_password_hash
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask import jsonify
+
 import datetime
 
 
@@ -64,19 +66,79 @@ class Note(db.Model):
         return 'Note {} {}'.format(self.tred_id, self.user_id)
 
 
-class PostModel:
-    def __init__(self, connection):
-        self.connection = connection
+def note_insert(content, tred_id, user_id):
+    user = get_user(user_id)
+    if user is None:
+        return jsonify({'error': 'User not found'})
+    tred = get_tred(tred_id)
+    if tred is None:
+        return jsonify({'error': 'Tred not found'})
+    note = Note(text=content, date=datetime.datetime.now())
+    user.notes.append(note)
+    tred.notes.append(note)
+    db.session.commit()
+    return jsonify({'success': 'OK'})
 
-    def insert(self, content, tred_id, user_id):
-        user = User.query.filter_by(id=user_id).first()
-        tred = Tred.query.filter_by(id=tred_id).first()
-        note = Note(text=content, date=datetime.datetime.now())
-        user.notes.append(note)
-        tred.notes.append(note)
+
+def tred_insert(topic, user_id):
+    user = get_user(user_id)
+    if user is None:
+        return jsonify({'error': 'User not found'})
+    tred = Tred(topic=topic, date=datetime.datetime.now())
+    user.treds.append(tred)
+    db.session.commit()
+    return jsonify({'success': 'OK'})
+
+
+def note_delete(user_id, note_id):
+    # user = User.query.filter_by(id=user_id)
+    note = get_note(note_id)
+    if note is None:
+        return jsonify({'error': 'Note not found'})
+    if note.user_id == user_id:
+        db.session.delete(note)
         db.session.commit()
+        return jsonify({'success': 'OK'})
+    else:
+        return jsonify({'error': 'You dont have permission'})
 
-    
+
+def tred_delete(user_id, tred_id):
+    tred = get_tred(tred_id)
+    if tred is None:
+        return jsonify({'error': 'Tred not found'})
+    if tred.user_id == user_id:
+        db.session.delete(tred)
+        db.session.commit()
+        return jsonify({'success': 'OK'})
+    else:
+        return jsonify({'error': 'You dont have permission'})
+
+
+def get_note(note_id):
+    return Note.query.filter_by(id=note_id).first()
+
+
+def get_tred(tred_id):
+    return Tred.query.filter_by(id=tred_id).first()
+
+
+def get_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+def get_all_treds():
+    return Tred.query.all()
+
+
+def get_all_notes(tred_id):
+    return Note.query.filter_by(tred_id=tred_id).all()
+
 
 db.create_all()
 db.session.commit()
+
+session = {
+    'username': None,
+    'user_id': None
+}
